6f040b16039902db53dd67ff643f8bd6b4188fca
laur3nfost3r <156472413+laur3nfost3r@users.noreply.github.com>
2024-01-16 18:12:59 +0000

    fixing incorrect lines

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ import streamlit as st
 import pandas as pd 
 import numpy as np
 st.title('practice app')
-streamlit run main.py
-streamlit run https://raw.githubusercontent.com/streamlit/demo-practice-app/master/main.py
+
