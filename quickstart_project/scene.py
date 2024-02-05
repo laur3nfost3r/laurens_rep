16883635357e62ebd5d34777160d452a96e94873
laur3nfost3r <156472413+laur3nfost3r@users.noreply.github.com>
2024-02-05 20:32:12 +0000

    discovered two classes makes two videos

--- quickstart_project/scene.py
+++ quickstart_project/scene.py
@@ -7,6 +7,22 @@ class CreateCircle(Scene):
         circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
         self.play(Create(circle))  # show the circle on screen
 
-c = CreateCircle()
-c.render()
-st.video("media/videos/1080p60/CreateCircle.mp4")
+class Shapes(Scene):
+    def construct(self):
+        triangle = Triangle()
+        self.play(Create(triangle))
+
+
+
+def main():
+
+    scene = CreateCircle()
+    scene.render()
+    st.video("media/videos/1080p60/CreateCircle.mp4")
+
+    next = Shapes()
+    next.render()
+    st.video("media/videos/1080p60/CreateCircle.mp4")
+
+if __name__=="__main__":
+    main()
