3f736beb571ef49ec6671e53f896920c6d49678c
laur3nfost3r <156472413+laur3nfost3r@users.noreply.github.com>
2024-02-05 20:16:04 +0000

    made line, confusion with multiple animations at once

---
+++ quickstart_project/shapes.py
@@ -0,0 +1,33 @@
+from manim import *
+import streamlit as st
+
+class Shapes(Scene):
+    def construct(self):
+        line = Line()
+        line.set_fill(BLUE, opacity=0.5)
+
+        circle = Circle()
+        circle.set_fill(GREEN, opacity = 0.5)
+        
+
+        line.shift(LEFT)
+        self.play(Create(line))
+        
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # create a square
+        square.rotate(PI / 4)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # fade out animation
+
+c = Shapes()
+d = SquareToCircle
+d.render
+c.render()
+st.video("media/videos/1080p60/Shapes.mp4")
+
